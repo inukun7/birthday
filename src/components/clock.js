ca35b66d3233917631e22a6c03f0c8a8705dfeeb
inukun7 <inukun716@gmail.com>
2018-07-26 08:57:25 -0600

    detailed styles

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,21 +4,21 @@ class Clock extends Component {
     render() {
         return (
             <div className="clock">
-                <div className="clock__days">
-                    <label ClassName="clock__title">DAYS</label>
-                    <label ClassName="clock__amount">20</label>
+                <div className="clock__days clock__box">
+                    <label className="clock__title">DAYS</label>
+                    <label className="clock__amount">20</label>
                 </div>
-                <div className="clock__hours">
-                    <label ClassName="clock__title">HOURS</label>
-                    <label ClassName="clock__amount">20</label>
+                <div className="clock__hours clock__box">
+                    <label className="clock__title">HOURS</label>
+                    <label className="clock__amount">20</label>
                 </div>
-                <div className="clock__minutes">
-                    <label ClassName="clock__title">MINUTES</label>
-                    <label ClassName="clock__amount">20</label>
+                <div className="clock__minutes clock__box">
+                    <label className="clock__title">MINUTES</label>
+                    <label className="clock__amount">20</label>
                 </div>
-                <div className="clock__seconds">
-                    <label ClassName="clock__title">SECONDS</label>
-                    <label ClassName="clock__amount">20</label>
+                <div className="clock__seconds clock__box">
+                    <label className="clock__title">SECONDS</label>
+                    <label className="clock__amount">20</label>
                 </div>
             </div>
         )
